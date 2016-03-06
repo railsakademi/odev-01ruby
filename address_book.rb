986657428b9f91e4e71821302f53b623b409ce70
Mehmet Beydogan <mehmet.beydogan@gmail.com>
2016-03-06 22:15:57 +0200

    Initialize documentation and base code for assignment

---
+++ address_book.rb
@@ -0,0 +1,13 @@
+class AddressBook
+  def initialize(csv_path)
+
+  end
+
+  def print_people
+
+  end
+
+  def search_person(person_name)
+
+  end
+end
---
+++ main.rb
@@ -0,0 +1,6 @@
+require './address_book.rb'
+require './person.rb'
+
+address_book = AddressBook.new("people.csv")
+address_book.print_people
+address_book.search_person("Michael")
