5e691a1e13c7f021545854c617b7e745e47dfb72
bilal arslan <bilal.arslan1993@hotmail.com>
2016-03-07 02:54:37 +0200

    Homework is completed
    For OSX; bundler gem installed. Changed minitest version with (5.8.4)

--- address_book.rb
+++ address_book.rb
@@ -1,13 +1,29 @@
-class AddressBook
-  def initialize(csv_path)
+require 'csv'
+require './person.rb'
 
+class AddressBook
+  attr_accessor :people
+
+  def initialize(csv_path)
+    @people = []
+  	CSV.foreach(csv_path) do |row|
+  	  people.push(Person.new(row[0], row[1], row[2], row[3]))
+  	end
   end
 
   def print_people
-
+  	people.each do |person|
+  	  puts "# #{person.id},#{person.full_name},#{person.phone_number},#{person.city}"
+  	end
   end
 
   def search_person(person_name)
-
+  	people.each do |person|
+  	  if person.full_name.to_s.include? person_name
+  	  	puts "# #{person.id},#{person.full_name},#{person.phone_number},#{person.city}"
+  	  	return
+  	  end
+  	end
+  	puts "#{person_name} is can\'t find!"
   end
 end
--- person.rb
+++ person.rb
@@ -1,3 +1,13 @@
 class Person
+  attr_accessor :id
+  attr_accessor :full_name
+  attr_accessor :phone_number
+  attr_accessor :city
 
+  def initialize(id, full_name, phone_number, city)
+    @id = id
+    @full_name = full_name
+    @phone_number = phone_number
+    @city = city
+  end
 end
