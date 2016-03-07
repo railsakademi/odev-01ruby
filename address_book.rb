20833241356abb9f0132ac5ddb462118cc3b70fc
Victor Tomaili <i@vctor.net>
2016-03-07 12:00:54 +0200

    Yay!

--- address_book.rb
+++ address_book.rb
@@ -1,13 +1,29 @@
-class AddressBook
-  def initialize(csv_path)
+require 'csv'
+require './person.rb'
 
+
+class AddressBook
+  attr_accessor :peoples
+  def initialize(csv_path)
+    @peoples = {}
+    csv = CSV.read(csv_path)
+    csv.shift
+    csv.each do |csvRow|
+        @peoples[csvRow[0]] = Person.new(csvRow[0], csvRow[1], csvRow[2], csvRow[3])
+    end
   end
 
   def print_people
-
+    @peoples.each do |key, person|
+        puts "#{person.id},#{person.full_name},#{person.phone_number},#{person.city}"
+    end
   end
 
   def search_person(person_name)
-
+    @peoples.each do |key, person|
+        if person.full_name.include? person_name
+            puts "#{person.id},#{person.full_name},#{person.phone_number},#{person.city}"
+        end
+    end
   end
 end
--- person.rb
+++ person.rb
@@ -1,3 +1,10 @@
 class Person
+    attr_accessor :id, :full_name, :phone_number, :city
 
+    def initialize(id, fullName, phoneName, city)
+        @id = id
+        @full_name = fullName
+        @phone_number = phoneName
+        @city = city
+    end
 end
