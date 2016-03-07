25d1543281fc906e0ed37b0c0ae2756c46e73b84
Erkam Kucet <Macbook@Erkams-MacBook-Pro.local>
2016-03-07 16:31:06 +0200

    First

--- address_book.rb
+++ address_book.rb
@@ -1,13 +1,26 @@
-class AddressBook
-  def initialize(csv_path)
+require 'csv'
 
+class AddressBook
+
+  @people = Array.new
+
+  def initialize(csv_path)
+    @people = CSV.read(csv_path)
   end
 
   def print_people
-
+    @people.each do |person|
+      print person
+      puts
+    end
   end
 
   def search_person(person_name)
-
+    if @people.include?(person_name)
+      puts "bulundu"
+      return 1
+    else
+      puts "bulunamadı"
+    end
   end
 end
--- main.rb
+++ main.rb
@@ -3,4 +3,4 @@ require './person.rb'
 
 address_book = AddressBook.new("people.csv")
 address_book.print_people
-address_book.search_person("Michael")
+address_book.search_person("Roy")
