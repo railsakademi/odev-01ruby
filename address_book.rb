2188f9c0efdecc30f03e62bffb2b87fb64b3cbb7
Erkam Kucet <Macbook@Erkams-MacBook-Pro.local>
2016-03-07 16:54:24 +0200

    Final

--- address_book.rb
+++ address_book.rb
@@ -1,26 +1,30 @@
 require 'csv'
+require './person.rb'
 
 class AddressBook
 
-  @people = Array.new
+  attr_accessor :people
 
   def initialize(csv_path)
-    @people = CSV.read(csv_path)
+    @people = []
+    CSV.foreach(csv_path) do |row|
+      people.push(Person.new(row[0], row[1], row[2],row[3]))
+    end
   end
 
   def print_people
-    @people.each do |person|
-      print person
-      puts
+    people.each do |person|
+      puts "#{person.id}, #{person.full_name}, #{person.phone_number}, #{person.city}"
     end
   end
 
   def search_person(person_name)
-    if @people.include?(person_name)
-      puts "bulundu"
-      return 1
-    else
-      puts "bulunamadı"
+    people.each do |person|
+      if person.full_name.to_s.include? person_name
+        puts "#{person.id}, #{person.full_name}, #{person.phone_number}, #{person.city}"
+        return
+      end
     end
+    puts "#{person_name} bulunamadı."
   end
 end
