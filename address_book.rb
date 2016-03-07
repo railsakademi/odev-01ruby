a26f39a9f20f42108c774f287b51b17919ec5b78
selimssevgi <selimssevgi@gmail.com>
2016-03-07 17:03:10 +0200

    complete first assignment

--- address_book.rb
+++ address_book.rb
@@ -1,13 +1,45 @@
 class AddressBook
-  def initialize(csv_path)
 
+  def initialize(csv_path)
+    @people = []
+    populatePeopleArray(csv_path)
   end
 
   def print_people
+    @people.each do |person|
+      print_person person
+    end
+  end
 
+  def print_person person
+    print "#{person.id},"
+    print "#{person.full_name},"
+    print "#{person.phone_number},"
+    puts  "#{person.city}"
   end
 
   def search_person(person_name)
-
+    @people.each do |person|
+      if person.full_name.include?(person_name)
+        print_person person
+      end
+    end
   end
+
+  private 
+    def populatePeopleArray(csv_path)
+      fh = open(csv_path)
+      fh.readline # read the header
+      while (line = fh.gets) != nil
+        line.chomp! # remove new line character
+        infoArray = line.split(',')
+        person = Person.new
+        person.id = infoArray[0]
+        person.full_name = infoArray[1]
+        person.phone_number = infoArray[2]
+        person.city = infoArray[3]
+        @people.push person
+      end
+    end   
 end
+    
--- person.rb
+++ person.rb
@@ -1,3 +1,3 @@
 class Person
-
+  attr_accessor :id, :full_name, :phone_number, :city
 end
