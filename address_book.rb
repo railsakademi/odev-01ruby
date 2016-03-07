b06549c54dbde209d12c330ba44881f47ea7bb9d
Furkan Celik <furkan.celik32@gmail.com>
2016-03-08 01:39:38 +0200

    AdressBook Sınıfı düzenlendi

--- address_book.rb
+++ address_book.rb
@@ -1,20 +1,28 @@
 require 'csv'
+
 class AddressBook
-  
+  attr_accessor :people
+
   def initialize(csv_path)
-    @csv_path = csv_path
+    @people = []
+    CSV.foreach(csv_path) do |row|
+      @people.push(Person.new(row[0], row[1], row[2],row[3]))
+    end
   end
 
   def print_people
-    CSV.foreach(@csv_path) do |row|
-      print row.to_csv
+    @people.each do |person|
+       puts print_row_person person
     end
   end
 
   def search_person(person_name)
-    CSV.foreach(@csv_path) do |row|
-       print row.to_csv if person_name == row[1].split(" ")[0]
+    @people.each do |person|
+       puts print_row_person(person) if  person.full_name.include? person_name
     end
   end
 
+  def print_row_person(person)
+    "#{person.id}, #{person.full_name}, #{person.phone_number}, #{person.city}"
+  end
 end
