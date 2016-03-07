524acfa614de45f98e8a3ddb1cf4ff988e3b7a9e
Furkan Celik <furkan.celik32@gmail.com>
2016-03-08 01:05:02 +0200

    AdressBook sınıfı eklendi ve main dosyası duzenlendi

--- address_book.rb
+++ address_book.rb
@@ -1,13 +1,20 @@
+require 'csv'
 class AddressBook
+  
   def initialize(csv_path)
-
+    @csv_path = csv_path
   end
 
   def print_people
-
+    CSV.foreach(@csv_path) do |row|
+      print row.to_csv
+    end
   end
 
   def search_person(person_name)
-
+    CSV.foreach(@csv_path) do |row|
+       print row.to_csv if person_name == row[1].split(" ")[0]
+    end
   end
+
 end
