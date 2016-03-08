87911af13d3de5900805a5a91a010bb6cb4fbe83
Selcuk OZTURK <selcuk@hotmail.com.tr>
2016-03-08 13:39:57 +0200

    Ödev #1 Tamam

--- person.rb
+++ person.rb
@@ -1,3 +1,16 @@
 class Person
+  attr_accessor :id, :full_name, :phone_number, :city
+
+  def initialize(id, full_name, phone_number, city)
+    @id = id
+    @full_name = full_name
+    @phone_number = phone_number
+    @city = city
+  end
+
+  def to_s
+    "#{id},#{full_name},#{phone_number},#{city}"
+  end
+
 
 end
