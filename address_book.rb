573561585940351498c8a2f9db426226f6488b37
Burak KIYAK <brkkyk@gmail.com>
2016-03-08 19:09:41 +0200

    cihankursun

--- address_book.rb
+++ address_book.rb
@@ -1,13 +1,26 @@
 class AddressBook
   def initialize(csv_path)
-
+    require 'csv'
+    @@book = CSV.read(csv_path)
   end
 
   def print_people
-
-  end
+    puts "Kisi listesi"
+    puts "-------------"
+   @@book.each do |people| 
+     puts people[0]+" "+people[1]+" "+people[2]+" "+people[3]
+   end
+   puts "--------------"
+ end
 
   def search_person(person_name)
-
+    puts "Aranilan kisi: " + person_name
+    found_people = "Kisi bulunamadi."
+    @@book.each do |people|
+      if people[1].include?person_name
+        found_people = people
+      end
+    end
+    puts found_people
   end
 end
