fe26862d29e3355c5bab3677c659e927c4d6d4de
cagdassalur <cagdassalur@gmail.com>
2016-03-09 16:44:30 +0200

    person.rb

--- address_book.rb
+++ address_book.rb
@@ -1,18 +1,22 @@
 require 'csv'
+require './person'
 
 class AddressBook
 	attr_accessor :people
   def initialize(csv_path)
-  	@people = CSV.read(csv_path)[1..-1]
+    @people = []
+  	CSV.read(csv_path)[1..-1].each do |p|
+      @people.push(Person.new(p[0],p[1],p[2],p[3]))
+    end
   end
 
   def print_people
   	@people.each do |p|
-  		puts p.join(',')
+  		puts p.to_s
   	end
   end
 
   def search_person(person_name)
-  	puts @people.find{|p| p[1].include?person_name}.join(',')
+  	puts @people.find{|p| p.full_name.include?person_name}.to_s
   end
 end
