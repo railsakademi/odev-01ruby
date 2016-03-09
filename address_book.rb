c0153fd716246518ea23b53022ff6dda48642741
cagdassalur <cagdassalur@gmail.com>
2016-03-09 11:14:27 +0200

    -

--- address_book.rb
+++ address_book.rb
@@ -1,13 +1,18 @@
-class AddressBook
-  def initialize(csv_path)
+require 'csv'
 
+class AddressBook
+	attr_accessor :people
+  def initialize(csv_path)
+  	@people = CSV.read(csv_path)[1..-1]
   end
 
   def print_people
-
+  	@people.each do |p|
+  		puts p.join(',')
+  	end
   end
 
   def search_person(person_name)
-
+  	puts @people.find{|p| p[1].include?person_name}.join(',')
   end
 end
