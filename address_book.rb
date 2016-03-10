247cc14bba105e40551204dbae88fb7771412983
wemrekurt <emre.kurt@bil.omu.edu.tr>
2016-03-10 20:56:45 +0200

    search olayi degisti

--- address_book.rb
+++ address_book.rb
@@ -11,21 +11,21 @@ class AddressBook
   end
   
   def beautifier alist
-    @people = {}
+    @people = []
     alist.each do |i|
-      key = i[1].split ' '
-      people[key[0]] = Person.new i[0],i[1],i[2],i[3]
+      people.push(Person.new i[0],i[1],i[2],i[3])
     end
   end
     
   def print_people
-    people.each do |k,v|
+    people.each do |v|
       puts "#{v.id},#{v.full_name},#{v.city},#{v.phone_number}"
     end
   end
 
   def search_person person_name
-    r = people[person_name]
-    puts "#{r.id},#{r.full_name},#{r.city},#{r.phone_number}"
+    people.each do |v|
+      puts "#{v.id},#{v.full_name},#{v.city},#{v.phone_number}" if v.full_name.include? person_name
+    end
   end
 end
